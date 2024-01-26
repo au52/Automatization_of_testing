f9ef268ffe577565a49106ea1633aeb6ab6bd0a6
Артем Уханов <au20ru@gmail.com>
2024-01-26 20:25:43 +0300

    Add files via upload

---
+++ Lesson5/task6.py
@@ -0,0 +1,12 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver.get('http://the-internet.herokuapp.com/entry_ad')
+sleep(2)
+driver.find_element(By.CSS_SELECTOR, 'div.modal-footer').click()
+sleep(2)
+driver.quit()
