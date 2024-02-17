45a81c197e578f99650416c1005266073e91e495
Артем Уханов <au20ru@gmail.com>
2024-02-17 20:35:49 +0300

    Add files via upload

--- Lesson6/task1.py
+++ Lesson6/task1.py
@@ -12,9 +12,11 @@ driver.get('http://uitestingplayground.com/ajax')
 driver.find_element(By.CSS_SELECTOR, '#ajaxButton').click()
 
 try:
-    element = WebDriverWait(driver, 20).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, 'p.bg-success'))
-    )
+    # element = WebDriverWait(driver, 20).until(
+    #     EC.presence_of_element_located((By.CSS_SELECTOR, 'p.bg-success'))
+    # )
+
+    driver.implicitly_wait(20)
     print(driver.find_element(By.CSS_SELECTOR, 'p.bg-success').text)
 
 except:
